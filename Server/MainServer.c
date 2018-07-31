40c90740458beb7156652baef9d53170b61e2fed
hp <hp@hp-VirtualBox>
2018-07-31 20:32:37 +0100

    Primeiro Commit

---
+++ Server/MainServer.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   MainServer.c
+ * Author: hp
+ *
+ * Created on 31 de Julho de 2018, 20:30
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
